7f29aee7efc48da5572035f2dd98f2d5b016f22a
Jarvis Brown <brownjarvis21@gmail.com>
2025-01-30 19:50:01 -0500

    styling home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+//import Image from "next/image";
+import Link from "next/link";
+import styles from "@/_styles/home.module.css";
 
 export default function Home() {
-    return <></>;
+    return (
+        <>
+            <main className={styles.main}>
+                <div className={styles.main_container}>
+                    <div>
+                        <h1 className={styles.header}>Title of App</h1>
+                        <div className={styles.btn_container}>
+                            <Link href="#">
+                                <button>Login</button>
+                            </Link>
+                            <Link href="#">
+                                <button>Create Account</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
